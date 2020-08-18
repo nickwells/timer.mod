2c56580ba90b46f89fe716794b0863b4fdc58a9b
Nick Wells <wells.nicholas@gmail.com>
2020-08-18 22:02:36 +0100

    Add the StartF function taking a function to call instead of an Actor

--- timer/example_test.go
+++ timer/example_test.go
@@ -1,6 +1,7 @@
 package timer_test
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/nickwells/timer.mod/timer"
@@ -16,4 +17,14 @@ func ExampleStart() {
 	time.Sleep(1 * time.Second)
 	// Output:
 	// ExampleStart: 1 second
+}
+
+func ExampleStartF() {
+	defer timer.StartF("ExampleStartF",
+		func(tag string, d time.Duration) {
+			fmt.Printf("%s: %d second\n", tag, d/time.Second)
+		})()
+	time.Sleep(1 * time.Second)
+	// Output:
+	// ExampleStartF: 1 second
 }
--- timer/timer.go
+++ timer/timer.go
@@ -10,12 +10,27 @@ type Actor interface {
 }
 
 // Start returns a function that should be called at the end of the period to
-// be timed (typically through use of the defer mechanism)
+// be timed (typically through use of the defer mechanism). When you call the
+// returned function it will calculate the duration since Start was called
+// and pass that with the tag to the Act method of the supplied Actor.
 func Start(tag string, a Actor) func() {
 	start := time.Now()
 
 	return func() {
 		d := time.Since(start)
 		a.Act(tag, d)
+	}
+}
+
+// StartF returns a function that should be called at the end of the period
+// to be timed (typically through use of the defer mechanism). When you call
+// the returned function it will calculate the duration since StartF was
+// called and pass that with the tag to the supplied function, f
+func StartF(tag string, f func(string, time.Duration)) func() {
+	start := time.Now()
+
+	return func() {
+		d := time.Since(start)
+		f(tag, d)
 	}
 }
